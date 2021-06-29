accf02f6e536a74294027da7ac27337e1f1235fb
norbutasvi <norbutasvilius@gmail.com>
2021-06-29 11:21:06 +0300

    test

--- services/getUrl.js
+++ services/getUrl.js
@@ -2,6 +2,6 @@ export function getUrl() {
     if (process.env.NODE_ENV == 'development') {
         return 'http://localhost:1337';
     } else {
-        return 'https://pilviskiai-admin.herokuapp.com';
+        return 'https://pilviskiai-adminarea.herokuapp.com';
     }
 }
